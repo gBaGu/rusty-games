90ae2faef9678c0e966573cfe77c4eb6cb5eb402
vadim <vadim.pysarenko97@gmail.com>
2024-03-25 09:51:17 +0200

    add conversion from PoisonError for GameStorageError

--- src/rpc_server/game_storage.rs
+++ src/rpc_server/game_storage.rs
@@ -1,7 +1,7 @@
 use crate::game::tic_tac_toe::{FieldCoordinates, GameState, PlayerId, TicTacToe, TicTacToeError};
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
-use std::sync::Mutex;
+use std::sync::{Mutex, PoisonError};
 
 #[derive(thiserror::Error, Debug)]
 pub enum GameStorageError {
@@ -11,10 +11,18 @@ pub enum GameStorageError {
     DuplicateGame,
     #[error("game with this id doesn't exist: {}", .id)]
     NoSuchGame { id: PlayerId },
-    #[error("failed to lock inner mutex")]
-    MutexLockFailed,
+    #[error("failed to lock inner mutex: {}", .description)]
+    MutexPoisonError { description: String },
     #[error(transparent)]
     TicTacToeError(#[from] TicTacToeError),
+}
+
+impl<T> From<PoisonError<T>> for GameStorageError {
+    fn from(value: PoisonError<T>) -> Self {
+        Self::MutexPoisonError {
+            description: value.to_string(),
+        }
+    }
 }
 
 #[derive(Debug, Default)]
@@ -28,7 +36,7 @@ impl GameStorage {
         player1: PlayerId,
         player2: PlayerId,
     ) -> Result<(), GameStorageError> {
-        let mut games_guard = self.games.lock().map_err(|_| GameStorageError::MutexLockFailed)?;
+        let mut games_guard = self.games.lock()?;
         match games_guard.entry(player1) {
             Entry::Vacant(e) => {
                 let game = TicTacToe::new(player1, player2)?;
@@ -45,7 +53,7 @@ impl GameStorage {
         player_id: PlayerId,
         coords: FieldCoordinates,
     ) -> Result<GameState, GameStorageError> {
-        let mut games_guard = self.games.lock().map_err(|_| GameStorageError::MutexLockFailed)?;
+        let mut games_guard = self.games.lock()?;
         let game = games_guard
             .get_mut(&game_id)
             .ok_or(GameStorageError::NoSuchGame { id: game_id })?;
@@ -53,7 +61,7 @@ impl GameStorage {
     }
 
     pub fn delete_game(&self, game_id: PlayerId) -> Result<(), GameStorageError> {
-        let mut games_guard = self.games.lock().map_err(|_| GameStorageError::MutexLockFailed)?;
+        let mut games_guard = self.games.lock()?;
         if let Entry::Occupied(e) = games_guard.entry(game_id) {
             if !e.get().is_finished() {
                 return Err(GameStorageError::DeleteActiveGameFailed);
