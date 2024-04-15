d7dc2464b97c39b19d91497afa48dfbf9a16b9fa
vadim <vadim.pysarenko97@gmail.com>
2024-04-15 18:22:59 +0300

    fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
-mod game;
-mod rpc_server;
-
-use game::tic_tac_toe::{FieldCol, FieldCoordinates, FieldRow, TicTacToe};
+use mp_game::game::game::Game;
+use mp_game::game::tic_tac_toe::{FieldCol, FieldRow, TicTacToe};
 
 fn main() {
     let player1 = 1;
     let player2 = 2;
-    let mut ttt = TicTacToe::new(player1, player2).unwrap();
-    ttt.make_turn(player1, FieldCoordinates::new(FieldRow::R2, FieldCol::C2))
-        .unwrap();
+    let mut ttt = TicTacToe::new(&[player1, player2]).unwrap();
+
+    let mut turn_data;
+    turn_data = <TicTacToe as Game>::TurnData::new(FieldRow::R2, FieldCol::C2);
+    ttt.update(player1, turn_data).unwrap();
     println!("{:?}", ttt);
-    ttt.make_turn(player2, FieldCoordinates::new(FieldRow::R2, FieldCol::C3))
-        .unwrap();
+    turn_data = <TicTacToe as Game>::TurnData::new(FieldRow::R2, FieldCol::C3);
+    ttt.update(player2, turn_data).unwrap();
     println!("{:?}", ttt);
-    ttt.make_turn(player1, FieldCoordinates::new(FieldRow::R3, FieldCol::C3))
-        .unwrap();
+    turn_data = <TicTacToe as Game>::TurnData::new(FieldRow::R3, FieldCol::C3);
+    ttt.update(player1, turn_data).unwrap();
     println!("{:?}", ttt);
-    ttt.make_turn(player2, FieldCoordinates::new(FieldRow::R1, FieldCol::C1))
-        .unwrap();
+    turn_data = <TicTacToe as Game>::TurnData::new(FieldRow::R1, FieldCol::C1);
+    ttt.update(player2, turn_data).unwrap();
     println!("{:?}", ttt);
-    ttt.make_turn(player1, FieldCoordinates::new(FieldRow::R3, FieldCol::C2))
-        .unwrap();
+    turn_data = <TicTacToe as Game>::TurnData::new(FieldRow::R3, FieldCol::C2);
+    ttt.update(player1, turn_data).unwrap();
     println!("{:?}", ttt);
-    ttt.make_turn(player2, FieldCoordinates::new(FieldRow::R1, FieldCol::C2))
-        .unwrap();
+    turn_data = <TicTacToe as Game>::TurnData::new(FieldRow::R1, FieldCol::C2);
+    ttt.update(player2, turn_data).unwrap();
     println!("{:?}", ttt);
-    ttt.make_turn(player1, FieldCoordinates::new(FieldRow::R3, FieldCol::C1))
-        .unwrap();
+    turn_data = <TicTacToe as Game>::TurnData::new(FieldRow::R3, FieldCol::C1);
+    ttt.update(player1, turn_data).unwrap();
     println!("{:?}", ttt);
 }
